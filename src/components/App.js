58cdccff4baf3504abe0cf218e3636f743329d0e
kawatech002 <kawanjiku94@gmail.com>
2023-11-08 14:08:49 +0300

    add ArticleList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App(props) {
   return (
     <div className="App">
       <Header/>
       <About/>
+      <ArticleList/>
     </div>
   );
 }
